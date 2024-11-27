59cefd93bcd79601c2455108a0ecf48745abe27e
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-27 09:29:07 +0900

    Create cwe.js

---
+++ cwe.js
@@ -0,0 +1,16 @@
+// This shows examples related with the Common Weakness Enumeration (CWE).
+
+function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3;
+    }
+}
+
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
