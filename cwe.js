fc62fc0eed86abaab0a3cf03d6533ccad63fcf12
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-27 13:15:14 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -1,11 +1,11 @@
 // This shows examples related with the Common Weakness Enumeration (CWE).
 
-function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
-    var arr = [1, 2, 3];
-    if (x < 0) {
-        arr[x] = 3;
-    }
-}
+// function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+//     var arr = [1, 2, 3];
+//     if (x < 0) {
+//         arr[x] = 3;
+//     }
+// }
 
 // function CWE_398() { // IDENTICAL_BRANCHES
 //     if (x >= 0) {
@@ -20,4 +20,9 @@ function CWE_476() { // NULL_POINTER
     // Add 1 line
     var y = obj.x;
     console.log(y);
+}
+
+function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
+    var x = -1;
+    if (x = -1) console.log('Error!', x);
 }
