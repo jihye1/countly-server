46f24e085be1c6dd6c60ae9bf2ca1e1be640b930
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-27 09:37:49 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -7,10 +7,17 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     }
 }
 
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
+// function CWE_398() { // IDENTICAL_BRANCHES
+//     if (x >= 0) {
+//         y = x;
+//     } else {
+//         y = x;
+//     }
+// }
+
+function CWE_476() { // NULL_POINTER
+    var obj;
+    // Add 1 line
+    var y = obj.x;
+    console.log(y);
 }
