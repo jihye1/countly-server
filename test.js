21c394e1ac85df59777799e1b79ecbd87cde8eb3
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-12-06 15:59:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-const a = 1;
-a = 3;
+function foo(){
+    const a = 1;
+    a = 3;
+}
+
+function poo(){
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3; // ARRAY_INDEX_NEGATIVE alarm: x is negative but is used as array index.
+    }
+}
